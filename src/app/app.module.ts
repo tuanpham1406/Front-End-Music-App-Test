1ab4340c5f7850c4a8a329cc0074867fc1ef23ba
DT2K50 <ndangduc.bn@gmail.com>
2019-08-09 16:20:36 +0700

    "USER-MANAGER: Created:  HeaderComponent"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './C.COMPONENT/2.UserManager/login/login.component';
+import { HeaderComponent } from './C.COMPONENT/1.Layout/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
