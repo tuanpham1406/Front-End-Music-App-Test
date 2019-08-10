ec778799ad5ad554c8cee00a720007e90d0b9bc0
DT2K50 <ndangduc.bn@gmail.com>
2019-08-10 12:37:16 +0700

    "SONG-CREATE: Updated Component for Song Feature"

--- src/app/C.COMPONENT/3.SongManager/create-song/create-song.component.ts
+++ src/app/C.COMPONENT/3.SongManager/create-song/create-song.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UploadFileService} from '../../../B.SERVICE/2.UploadFile/upload-file.service';
+import {HttpEventType, HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-create-song',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateSongComponent implements OnInit {
 
-  constructor() { }
+  selectedFiles: FileList;
+  currentFileUpload: File;
+  progress: { percentage: number } = { percentage: 0 };
+
+  constructor(private uploadService: UploadFileService) { }
 
   ngOnInit() {
+  }
+
+  selectFile(event) {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload() {
+    this.progress.percentage = 0;
+
+    this.currentFileUpload = this.selectedFiles.item(0);
+    this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progress.percentage = Math.round(100 * event.loaded / event.total);
+      } else if (event instanceof HttpResponse) {
+        console.log('File is completely uploaded!');
+      }
+    });
+
+    this.selectedFiles = undefined;
   }
 
 }
--- src/app/C.COMPONENT/3.SongManager/list-song/list-song.component.ts
+++ src/app/C.COMPONENT/3.SongManager/list-song/list-song.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {UploadFileService} from '../../../B.SERVICE/2.UploadFile/upload-file.service';
 
 @Component({
   selector: 'app-list-song',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-song.component.scss']
 })
 export class ListSongComponent implements OnInit {
+  showFile = false;
+  fileUploads: Observable<string[]>;
 
-  constructor() { }
+  constructor(private uploadService: UploadFileService) { }
 
   ngOnInit() {
   }
 
+  showFiles(enable: boolean) {
+    this.showFile = enable;
+
+    if (enable) {
+      this.fileUploads = this.uploadService.getFiles();
+    }
+  }
 }
