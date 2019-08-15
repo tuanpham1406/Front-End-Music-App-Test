10422f98fcb98e3e47ce4d5fdbee94138d81eab6
khuynhthanh <phamanhtuan1406@gmail.com>
2019-08-15 16:03:53 +0700

    layout: thêm list bài hát giả

--- src/app/C.COMPONENT/3.SongManager/list-song/list-song.component.ts
+++ src/app/C.COMPONENT/3.SongManager/list-song/list-song.component.ts
@@ -1,7 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
 import {SongService} from '../../../B.SERVICE/2.SongManager/song.service';
+
+interface MyPlaylist {
+  id?: number;
+  name: string;
+}
+
+const MYPLAYLIST: MyPlaylist[] = [
+  {name: 'Anh thương em nhất mà'},
+  {name: 'Tâm lặng như nước'},
+  {name: 'Xanh lục'},
+  {name: 'I wan\'t something just like this'},
+  {name: 'Một bước yêu vạn dặm đau'},
+];
 
 @Component({
   selector: 'app-list-song',
@@ -9,15 +20,31 @@ import {SongService} from '../../../B.SERVICE/2.SongManager/song.service';
   styleUrls: ['./list-song.component.scss']
 })
 export class ListSongComponent implements OnInit {
+  page = 1;
+  pageSize = 4;
+  collectionSize = MYPLAYLIST.length;
+
+  get myPlaylist(): MyPlaylist[] {
+    return MYPLAYLIST
+      .map((myPlaylist, i) => ({id: i + 1, ...myPlaylist}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
+
   listSong: any[];
-  constructor(private songService: SongService) { }
+
+  constructor(private songService: SongService) {
+  }
 
   ngOnInit() {
     this.songService
       .getSong()
       .subscribe(
-        data => {this.listSong = data; },
-        error => {console.log(error); }
+        data => {
+          this.listSong = data;
+        },
+        error => {
+          console.log(error);
+        }
       );
   }
 }
