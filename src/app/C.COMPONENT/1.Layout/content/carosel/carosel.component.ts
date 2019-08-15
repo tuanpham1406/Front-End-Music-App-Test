301c49679c674e18506bfc8640b611dde1f7998d
khuynhthanh <phamanhtuan1406@gmail.com>
2019-08-15 15:06:41 +0700

    layout: add link ảnh carousel trong component

--- src/app/C.COMPONENT/1.Layout/content/carosel/carosel.component.ts
+++ src/app/C.COMPONENT/1.Layout/content/carosel/carosel.component.ts
@@ -8,6 +8,11 @@ import {CaroselService} from '../../../../B.SERVICE/0.Layout/carosel.service';
   styleUrls: ['./carosel.component.scss']
 })
 export class CaroselComponent implements OnInit {
+  images = [1].map(() => `https://i.ytimg.com/vi/z8S7SZ4sJF0/maxresdefault.jpg`);
+  images1 = [2].map(() => `https://i.ytimg.com/vi/MIGCCcQkV-w/maxresdefault.jpg`);
+  images2 = [3].map(() => `https://i.ytimg.com/vi/jFzfkOulKEo/maxresdefault.jpg`);
+  images3 = [4].map(() => `https://reviewsmoi.com/wp-content/uploads/2019/03/mot-buoc-yeu-van-dam-dau-1.jpg`);
+  images4 = [5].map(() => `https://i.ytimg.com/vi/pz_23dIZgVQ/maxresdefault.jpg`);
   selectedFiles: FileList;
   currentFileUpload: FileUpload;
   progress: { percentage: number } = { percentage: 0 };
